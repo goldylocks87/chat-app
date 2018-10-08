db1ac28aa4b6563f108caa4808619238d0d34842
Rapunzel <ahhlick@gmail.com>
2018-10-07 18:14:01 -0700

    added socketIO connect & disconnect events

--- server/server.js
+++ server/server.js
@@ -1,13 +1,26 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
-const app = express();
 const port = process.env.PORT || 3000; 
+const app = express();
+const server = http.createServer(app); // spin up with express app
+const io = socketIO(server);
 
 // use path to normalize
 const publicPath = path.join(__dirname, '../public');
 
 app.use( express.static(publicPath) );
+
+// listen for an event
+io.on('connection', (socket) => {
+    console.log('New user connected...');
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected...')
+    });
+});
 
 app.get('/', (req, res) => {
     res.render('index.html');
@@ -15,6 +28,6 @@ app.get('/', (req, res) => {
 
 module.exports = { app };
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`started app on port ${port}...`);
 });
